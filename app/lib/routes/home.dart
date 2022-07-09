234c105a73096d577f8a7ee7bdb82acd0d2c7955
sujang958 <enter7377@naver.com>
2022-07-09 17:48:55 +0900

    pkg: add the semibold font

--- app/lib/routes/home.dart
+++ app/lib/routes/home.dart
@@ -11,8 +11,75 @@ class _HomeRouteState extends State<HomeRoute> {
   @override
   Widget build(BuildContext context) {
     return DefaultContainer(
-      child: Center(
-        child: Text("IH"),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 6.0),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Weather",
+                  style: TextStyle(fontSize: 35.0, fontWeight: FontWeight.bold),
+                ),
+                Material(
+                    color: Colors.transparent,
+                    child: IconButton(
+                        highlightColor: Colors.transparent,
+                        splashColor: Colors.transparent,
+                        onPressed: () {},
+                        icon: const Icon(
+                          CupertinoIcons.add,
+                          color: Colors.white,
+                        ))),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 15.4),
+            ),
+            Expanded(
+              child: CustomScrollView(
+                physics: const BouncingScrollPhysics(),
+                slivers: [
+                  CupertinoSliverRefreshControl(
+                    onRefresh: () async {},
+                  ),
+                  SliverList(
+                    delegate: SliverChildListDelegate([
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Suseong-gu",
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 16.0,
+                                  )),
+                              Text(
+                                "27 °C",
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 28.0,
+                                ),
+                              ), // this is bigger
+                            ],
+                          )
+                        ],
+                      ),
+                    ]),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
